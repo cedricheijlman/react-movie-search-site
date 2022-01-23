8ea1a6ec4dd4653544ab899972675815a9cac013
cedric <cheijlman@roc-dev.com>
2022-01-23 18:17:30 +0100

    castOverview

--- src/components/CastOverview.js
+++ src/components/CastOverview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CastOverview({ cast }) {
   const imageLink = "https://image.tmdb.org/t/p/original/";
@@ -8,17 +9,19 @@ function CastOverview({ cast }) {
       <div className="castOverview_row">
         {cast.slice(0, 14).map((person) => {
           return (
-            <div className="cast_card">
-              <img
-                src={
-                  person.profile_path == null
-                    ? "https://image.tmdb.org/t/p/original//VP6VDYL0OEDwSbTRysiJ0kAGxI.jpg"
-                    : imageLink + person.profile_path
-                }
-              />
-              <h2>{person.original_name}</h2>
-              <h3>{person.character}</h3>
-            </div>
+            <Link to={`/actors/${person.id}`}>
+              <div key={person.cast_id} className="cast_card">
+                <img
+                  src={
+                    person.profile_path == null
+                      ? "https://image.tmdb.org/t/p/original//VP6VDYL0OEDwSbTRysiJ0kAGxI.jpg"
+                      : imageLink + person.profile_path
+                  }
+                />
+                <h2>{person.original_name}</h2>
+                <h3>{person.character}</h3>
+              </div>
+            </Link>
           );
         })}
       </div>
